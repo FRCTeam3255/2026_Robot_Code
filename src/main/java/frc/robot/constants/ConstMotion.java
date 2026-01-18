46bd9d6aa57c91a2c96c3fb1939ca2d85877eaa2
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared@users.noreply.github.com>
2026-01-17 16:58:16 -0800

    we made the motors for this climber, pivot intake, and hood pivot.

--- src/main/java/frc/robot/constants/ConstMotion.java
+++ src/main/java/frc/robot/constants/ConstMotion.java
@@ -6,4 +6,13 @@ package frc.robot.constants;
 
 /** Add your docs here. */
 public class ConstMotion {
+  public static final double INTAKE_PIVOT_SPEED = 0.5;
+
+  public static final double EXTEND_CLIMBER_MOTOR_SPEED = 0;
+
+  public static final double STOP_CLIMBER_MOTOR = 0;
+
+  public static final double RETRACT_CLIMBER_MOTOR_SPEED = -0.5;
+
+  public static final double ANGLE_HOOD_SPEED = 0.5;
 }
--- src/main/java/frc/robot/subsystems/Motion.java
+++ src/main/java/frc/robot/subsystems/Motion.java
@@ -4,17 +4,37 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.DeviceIDs;
 
 @Logged
 public class Motion extends SubsystemBase {
   /** Creates a new Motion. */
+  final TalonFX IntakePivot = new TalonFX(DeviceIDs.motionIDs.INTAKE_PIVOT_CAN);
+  final TalonFX Climber = new TalonFX(DeviceIDs.motionIDs.CLIMBER_CAN);
+  final TalonFX Hood = new TalonFX(DeviceIDs.motionIDs.HOOD_CAN);
+
   public Motion() {
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+  }
+
+  public void pivotIntake(double speed) {
+    IntakePivot.set(speed);
+  }
+
+  public void startClimb(double speed) {
+    Climber.set(speed);
+  }
+
+  public void pivotHood(double speed) {
+    Hood.set(speed);
   }
 }
