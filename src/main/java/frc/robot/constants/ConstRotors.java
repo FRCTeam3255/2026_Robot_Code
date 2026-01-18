1ebe80c118dbef7e4d08b84909301c84ac2f0005
FRCTeam3255-Shared-K11-20 <frcteam.3255@gmail.com>
2026-01-17 18:51:41 -0800

    i fixed null problem
    -jayden

--- src/main/java/frc/robot/constants/ConstRotors.java
+++ src/main/java/frc/robot/constants/ConstRotors.java
@@ -9,10 +9,15 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 /** Add your docs here. */
 public class ConstRotors {
 
-  public static final TalonFXConfiguration SERIALIZER_ROLLERS_CONFIGURATION = null;
-  public static final TalonFXConfiguration INTAKE_ROLLERS_CONFIGURATION = null;
-  public static final TalonFXConfiguration SERIALIZER_V_FUNNEL_CONFIGURATION = null;
-  public static final TalonFXConfiguration SHOOTER_TRANSFER_CONFIGURATION = null;
-  public static final TalonFXConfiguration FLYWHEEL_LEFT_CONFIGURATION = null;
-  public static final TalonFXConfiguration FLYWHEEL_RIGHT_CONFIGURATION = null;
+  public static final TalonFXConfiguration SERIALIZER_ROLLERS_CONFIGURATION = new TalonFXConfiguration();
+  public static final TalonFXConfiguration INTAKE_ROLLER_CONFIGURATION = new TalonFXConfiguration();
+  public static final TalonFXConfiguration SERIALIZER_V_FUNNEL_CONFIGURATION = new TalonFXConfiguration();
+  public static final TalonFXConfiguration SHOOTER_TRANSFER_CONFIGURATION = new TalonFXConfiguration();
+  public static final TalonFXConfiguration FLYWHEEL_CONFIGURATION = new TalonFXConfiguration();
+  public static final double INTAKE_ROLLER_SPEED = 1;
+  public static final double SERIALIZER_V_FUNNEL_SPEED = 1;
+  public static final double SERIALIZER_ROLLERS_SPEED = 1;
+  public static final double SHOOTER_TRANSFER_SPEED = 1;
+  public static final double FLYWHEEL_SPEED = 1;
+
 }
