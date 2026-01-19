9e3c58ccf5bd45eaac3d762c264fbc0f47ecff61
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared@users.noreply.github.com>
2026-01-19 11:23:51 -0800

    added STOP constants

--- src/main/java/frc/robot/commands/ClimbingL2_3.java
+++ src/main/java/frc/robot/commands/ClimbingL2_3.java
@@ -29,7 +29,7 @@ public class ClimbingL2_3 extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    RobotContainer.motionInstance.setClimbSpeed(0);
+    RobotContainer.motionInstance.setClimbSpeed(ConstMotion.STOP);
   }
 
   // Returns true when the command should end.
