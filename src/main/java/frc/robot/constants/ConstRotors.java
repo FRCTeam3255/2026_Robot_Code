2e18c3cf418144be3c801c662575f18300bbfe14
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared@users.noreply.github.com>
2026-01-19 11:47:50 -0800

    added STOP constants

--- src/main/java/frc/robot/constants/ConstRotors.java
+++ src/main/java/frc/robot/constants/ConstRotors.java
@@ -17,7 +17,6 @@ public class ConstRotors {
   public static final TalonFXConfiguration SHOOTER_TRANSFER_CONFIGURATION = new TalonFXConfiguration();
   public static final TalonFXConfiguration FLYWHEEL_LEFT_CONFIGURATION = new TalonFXConfiguration();
   public static final TalonFXConfiguration FLYWHEEL_RIGHT_CONFIGURATION = new TalonFXConfiguration();
-  public static final double STOP = 0.0;
   public static final double INTAKE_ROLLER_SPEED = 1;
   public static final double SERIALIZER_V_FUNNEL_SPEED = 1;
   public static final double SERIALIZER_ROLLERS_SPEED = 1;
