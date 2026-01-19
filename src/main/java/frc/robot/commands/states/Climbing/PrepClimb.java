93e85c139cc5fd6297ad980e5347201b9928cfe2
BrodyKarr <boocat1030@icloud.com>
2026-01-19 10:42:22 -0800

    Fixed nulls

--- src/main/java/frc/robot/commands/states/Climbing/PrepClimb.java
+++ src/main/java/frc/robot/commands/states/Climbing/PrepClimb.java
@@ -5,28 +5,32 @@
 package frc.robot.commands.states.Climbing;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
+import frc.robot.constants.ConstDrivetrain;
+import frc.robot.constants.ConstField;
 import frc.robot.constants.ConstMotion;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Motion;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class PrepClimb extends Command {
-  Motion globalMotion;
   Drivetrain globalDrivetrain;
 
   /** Creates a new PrepClimb. */
-  public PrepClimb(Motion subMotion, Drivetrain subDrivetrain) {
-    globalMotion = subMotion;
+  public PrepClimb(Drivetrain subDrivetrain) {
     globalDrivetrain = subDrivetrain;
+
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    globalDrivetrain.rotationalAlign(null, null);
-    globalMotion.setIntakePivotSpeed(ConstMotion.RETRACT_INTAKE_PIVOT_SPEED);
-    globalMotion.setClimbSpeed(ConstMotion.EXTEND_CLIMBER_MOTOR_SPEED);
+    // TOdo: replace nulls with proper values
+    // globalDrivetrain.rotationalAlign(ConstField.TOWER_POSE,
+    // ConstDrivetrain.TURN_SPEED);
+    RobotContainer.motionInstance.setIntakePivotSpeed(ConstMotion.RETRACT_INTAKE_PIVOT_SPEED);
+    RobotContainer.motionInstance.setClimbSpeed(ConstMotion.EXTEND_CLIMBER_MOTOR_SPEED);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -37,7 +41,8 @@ public class PrepClimb extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    globalMotion.setClimbSpeed(0);
+    RobotContainer.motionInstance.setClimbSpeed(0);
+    RobotContainer.motionInstance.setIntakePivotSpeed(0);
   }
 
   // Returns true when the command should end.
