1efbe8ccc9e76fbac8a4519e80ffb90d73aa324a
FRCTeam3255-Shared-K1-10 <frcteam3255@gmail.com>
2026-01-31 16:51:04 -0800

    Update PrepClimbL1 State for transition (#122)
    * I added addrequirements, turned thing inside of isFinished to true
    
    * Updated ClimbingL1 State for transition
    
    Co-Authored-By: Justinpham17386769 <244249272+Justinpham17386769@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Motion.java
+++ src/main/java/frc/robot/subsystems/Motion.java
@@ -53,4 +53,16 @@ public class Motion extends SubsystemBase {
     climber.setControl(climberMotionRequest.withPosition(setpoint.in(Units.Inches)));
   }
 
+  public Distance getClimberDistance() {
+    return Units.Inches.of(climber.getPosition().getValueAsDouble());
+
+  }
+
+  public boolean isAtPosition(Distance desiredPos, Distance tolerance) {
+    Distance lowerLim = desiredPos.minus(tolerance);
+    Distance upperLim = desiredPos.plus(tolerance);
+
+    return getClimberDistance().gte(lowerLim)
+        && getClimberDistance().lte(upperLim);
+  }
 }
