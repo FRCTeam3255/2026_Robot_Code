06b12fdda452d229041009085ac516568fe4722c
Wu-Fan-529 <3681247216@qq.com>
2026-02-19 09:35:31 +0800

    Update ConstMotion.java
    Co-Authored-By: MiaResendiz123 <244249291+MiaResendiz123@users.noreply.github.com>

--- src/main/java/frc/robot/constants/ConstMotion.java
+++ src/main/java/frc/robot/constants/ConstMotion.java
@@ -25,7 +25,7 @@ public class ConstMotion {
   public static final TalonFXConfiguration CLIMBER_CONFIGURATION = new TalonFXConfiguration();
   public static final TalonFXConfiguration HOOD_CONFIGURATION = new TalonFXConfiguration();
   public static final double STOP = 0.0;
-  public static final Angle DEPLOY_INTAKE_PIVOT_ANGLE = Units.Degrees.of(135); // TODO: set angle for intake pivot
+  public static final Angle DEPLOY_INTAKE_PIVOT_ANGLE = Units.Degrees.of(126); // TODO: set angle for intake pivot
   public static final Angle RETRACT_INTAKE_PIVOT_ANGLE = Units.Degrees.of(0); // TODO: set angle for intake pivot
   public static final double EXTEND_CLIMBER_SPEED = 0;
   public static final double RETRACT_CLIMBER_SPEED = -0.5;
