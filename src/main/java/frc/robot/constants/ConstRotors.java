5ca189cb9d081f2c9b5dc7b074698d70f8e0a273
Wu-Fan-529 <3681247216@qq.com>
2026-02-22 04:34:26 +0800

    Update ConstRotors.java

--- src/main/java/frc/robot/constants/ConstRotors.java
+++ src/main/java/frc/robot/constants/ConstRotors.java
@@ -39,13 +39,13 @@ public class ConstRotors {
   }
 
   // different shooting locations shooting speeds
-  public static final AngularVelocity FLYWHEEL_TRENCH_SPEED = RPM.of(5600);
-  public static final AngularVelocity FLYWHEEL_OUTPOST_SPEED = RPM.of(5600);
-  public static final AngularVelocity FLYWHEEL_NON_OUTPOST_SPEED = RPM.of(5600);
-  public static final AngularVelocity FLYWHEEL_DEPOT_SPEED = RPM.of(5600);
-  public static final AngularVelocity FLYWHEEL_ANYWHERE_SPEED = RPM.of(5600);
-  public static final AngularVelocity FLYWHEEL_NEUTRAL_TO_ALLIANCE_SPEED = RPM.of(5600);
-  public static final AngularVelocity FLYWHEEL_OPPONENT_TO_ALLIANCE_SPEED = RPM.of(5600);
+  public static final AngularVelocity FLYWHEEL_TRENCH_SPEED = RPM.of(4300);
+  public static final AngularVelocity FLYWHEEL_OUTPOST_SPEED = RPM.of(4300);
+  public static final AngularVelocity FLYWHEEL_NON_OUTPOST_SPEED = RPM.of(4300);
+  public static final AngularVelocity FLYWHEEL_DEPOT_SPEED = RPM.of(4300);
+  public static final AngularVelocity FLYWHEEL_ANYWHERE_SPEED = RPM.of(4300);
+  public static final AngularVelocity FLYWHEEL_NEUTRAL_TO_ALLIANCE_SPEED = RPM.of(4300);
+  public static final AngularVelocity FLYWHEEL_OPPONENT_TO_ALLIANCE_SPEED = RPM.of(4300);
 
   public static final double REVERSE_INTAKE_ROLLER_SPEED = -1;
   public static final double REVERSE_SERIALIZER_V_FUNNEL_SPEED = 0;
@@ -94,9 +94,9 @@ public class ConstRotors {
     // flywheelSpeedMap.put(Inches.of(60).in(Inches), RPM.of(3200).in(RPM));
     // flywheelSpeedMap.put(Inches.of(90).in(Inches), RPM.of(3600).in(RPM));
     // flywheelSpeedMap.put(Inches.of(120).in(Inches), RPM.of(4100).in(RPM));
-    // flywheelSpeedMap.put(Inches.of(150).in(Inches), RPM.of(4500).in(RPM));
+    // flywheelSpeedMap.put(Inches.of(150).in(Inches), RPM.of(4300).in(RPM));
     // flywheelSpeedMap.put(Inches.of(187.8).in(Inches), RPM.of(5200).in(RPM));
-    flywheelSpeedMap.put(Inches.of(187.8).in(Inches), RPM.of(5600).in(RPM));
-    flywheelSpeedMap.put(Inches.of(0).in(Inches), RPM.of(5600).in(RPM));
+    flywheelSpeedMap.put(Inches.of(187.8).in(Inches), RPM.of(4300).in(RPM));
+    flywheelSpeedMap.put(Inches.of(0).in(Inches), RPM.of(4300).in(RPM));
   }
 }
