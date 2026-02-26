35c6021c41bfa828aa7a251c4af6fe086e8e2d4d
Evan Grinnell <worldevan8@gmail.com>
2026-02-25 17:41:04 -0800

    fixed build errors

--- src/main/java/frc/robot/commands/states/PrepShoots/PrepOutpost.java
+++ src/main/java/frc/robot/commands/states/PrepShoots/PrepOutpost.java
@@ -15,6 +15,8 @@ import frc.robot.subsystems.StateMachine;
 public class PrepOutpost extends BasePrepShooter {
   public PrepOutpost() {
     super(ConstRotors.FLYWHEEL_OUTPOST_SPEED, ConstMotion.HOOD_OUTPOST_ANGLE,
-        ConstPoseDrive.PrepShootRotations.OUTPOST_PREP_SHOOT_ROTATION, StateMachine.RobotState.PREP_OUTPOST);
+        ConstField.FieldElementGroups.OUTPOST_PREP_SHOOT_ROTATION_SET,
+        StateMachine.RobotState.PREP_OUTPOST);
+    // Use addRequirements() here to declare subsystem dependencies.
   }
 }
