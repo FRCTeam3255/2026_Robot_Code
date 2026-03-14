099d1e4583f6e2294da1881c26d8be905b35fb99
FRCTeam3255-Shared <frcteam3255@gmail.com>
2026-03-14 16:54:02 -0700

    Changed softlimit for the hood to be 0
    Co-Authored-By: Justinpham17386769 <244249272+Justinpham17386769@users.noreply.github.com>

--- src/main/java/frc/robot/constants/ConstMotion.java
+++ src/main/java/frc/robot/constants/ConstMotion.java
@@ -91,7 +91,7 @@ public class ConstMotion {
     HOOD_CONFIGURATION.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
     HOOD_CONFIGURATION.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
     HOOD_CONFIGURATION.SoftwareLimitSwitch.ForwardSoftLimitThreshold = Units.Degrees.of(46).in(Rotations);
-    HOOD_CONFIGURATION.SoftwareLimitSwitch.ReverseSoftLimitThreshold = Units.Degrees.of(1.6).in(Rotations);
+    HOOD_CONFIGURATION.SoftwareLimitSwitch.ReverseSoftLimitThreshold = Units.Degrees.of(0).in(Rotations);
     HOOD_CONFIGURATION.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
     HOOD_CONFIGURATION.Slot0.kS = 0.2;
     HOOD_CONFIGURATION.Slot0.kP = 300;
