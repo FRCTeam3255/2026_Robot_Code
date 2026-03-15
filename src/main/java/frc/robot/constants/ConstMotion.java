d191230b0dc16990fef3867d3e90a3b6eed1672c
MiaResendiz123 <miaperez1361@gmail.com>
2026-03-14 19:23:52 -0700

    renamed elevator to climber (#234)
    * renamed elevator to climber
    
    * Fixed renaming

--- src/main/java/frc/robot/constants/ConstMotion.java
+++ src/main/java/frc/robot/constants/ConstMotion.java
@@ -49,8 +49,8 @@ public class ConstMotion {
   public static final double LIFT_INTAKE_DELAY = 1.0;
   public static final double LIFT_INTAKE_INTERVAL_TIME = 1.0 / 3.0;
   public static final Angle INTAKE_PIVOT_ANGLE_TOLERANCE = Units.Degrees.of(5);
-  public static final int ELEVATOR_SLOW_PID_ID = 0;
-  public static final int ELEVATOR_FAST_PID_ID = 1;
+  public static final int CLIMBER_SLOW_PID_SLOT = 0;
+  public static final int CLIMBER_FAST_PID_SLOT = 1;
 
   public static final Distance CLIMBER_TOLERANCE = Inches.of(0.2);
   static {
