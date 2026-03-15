fa06401db785b60c34e87b985589900ad3eb0d75
BrodyKarr <boocat1030@icloud.com>
2026-03-14 19:12:51 -0700

    Made the other motors follow the speed of one accordingly

--- src/main/java/frc/robot/subsystems/Rotors.java
+++ src/main/java/frc/robot/subsystems/Rotors.java
@@ -67,9 +67,9 @@ public class Rotors extends SubsystemBase {
 
   public void setFlywheelPercentOutput(double percent) {
     flywheelTopEast.set(percent);
-    flywheelTopWest.set(percent);
-    flywheelBottomEast.set(percent);
-    flywheelBottomWest.set(percent);
+    flywheelTopWest.setControl(flywheelWestFollower);
+    flywheelBottomEast.setControl(flywheelEastFollower);
+    flywheelBottomWest.setControl(flywheelWestFollower);
     lastDesiredFlywheelSpeed = Units.RPM.of(5400 * percent);
   }
 
