dc57bb7a690e5d7e6fdab7a70fe1144c4094fecf
MiaResendiz123 <miaperez1361@gmail.com>
2026-03-17 20:02:29 -0700

    Tune new fixed shooting locations (#244)
    * Update ConstMotion.java
    
    * corner
    
    * hub
    
    ---------
    
    Co-authored-by: FRCTeam3255-Shared <170778697+FRCTeam3255-Shared@users.noreply.github.com>
    Co-authored-by: Wu-Fan-529 <3681247216@qq.com>

--- src/main/java/frc/robot/constants/ConstMotion.java
+++ src/main/java/frc/robot/constants/ConstMotion.java
@@ -32,11 +32,11 @@ public class ConstMotion {
   public static final double EXTEND_CLIMBER_SPEED = 0;
   public static final double RETRACT_CLIMBER_SPEED = -0.5;
   public static final double ANGLE_HOOD_SPEED = 0.5;
-  public static final Angle HOOD_TRENCH_ANGLE = Units.Degrees.of(13.5);
-  public static final Angle HOOD_CORNER_ANGLE = Units.Degrees.of(26);
+  public static final Angle HOOD_TRENCH_ANGLE = Units.Degrees.of(16);
+  public static final Angle HOOD_CORNER_ANGLE = Units.Degrees.of(27);
   public static final Angle HOOD_NON_OUTPOST_ANGLE = Units.Degrees.of(12);
   public static final Angle HOOD_TOWER_ANGLE = Units.Degrees.of(11);
-  public static final Angle HOOD_HUB_ANGLE = Units.Degrees.of(43); // TODO: adjust
+  public static final Angle HOOD_HUB_ANGLE = Units.Degrees.of(3.7); // TODO: adjust
   public static final Angle HOOD_DEPOT_ANGLE = Units.Degrees.of(18);
   public static final Angle HOOD_ANYWHERE_ANGLE = Units.Degrees.of(45);
   public static final Angle HOOD_NUETRAL_TO_ALLIANCE_ANGLE = Units.Degrees.of(45);
