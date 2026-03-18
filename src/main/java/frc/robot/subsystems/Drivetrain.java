b8459e1b182547aac20438e54e29fbdcbe79a84f
Wu-Fan-529 <3681247216@qq.com>
2026-03-18 10:51:31 +0800

    Update Drivetrain.java

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -33,6 +33,7 @@ import edu.wpi.first.units.measure.Distance;
 import frc.robot.DeviceIDs;
 import frc.robot.RobotContainer;
 import frc.robot.constants.ChoreoTraj;
+import frc.robot.constants.ChoreoVars;
 import frc.robot.constants.ConstDrivetrain;
 import frc.robot.constants.ConstPoseDrive.PoseDriveGroup;
 
@@ -221,7 +222,7 @@ public class Drivetrain extends SN_SuperSwerveV2 {
     Pose2d desiredTower = currentPose.nearest(TowerPoses);
     Distance distanceToTower = Units.Meters.of(currentPose.getTranslation().getDistance(desiredTower.getTranslation()));
     System.out.println("Distance to tower: " + distanceToTower.in(Units.Meters) + " meters");
-    if (desiredTower == TowerPoses.get(0)) {
+    if (desiredTower == ChoreoVars.Poses.D_side_climb_starting_pose) {
       return ChoreoTraj.DSideClimb;
     } else {
       return ChoreoTraj.OSideClimb;
