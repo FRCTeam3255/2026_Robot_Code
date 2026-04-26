6b7c43011119b47c1d6d8d2038f2b0718ec03222
Yutong Zhu <3681247216@qq.com>
2026-04-25 22:12:38 -0700

    Shots tuning pre wcmp (#303)
    * functional
    
    * close
    
    * Update ConstRotors.java
    
    * 70-130 good
    
    * 130-140 good
    
    * 200

--- src/main/java/frc/robot/constants/ConstRotors.java
+++ src/main/java/frc/robot/constants/ConstRotors.java
@@ -110,12 +110,16 @@ public class ConstRotors {
     // flywheelSpeedMap.put(Inches.of(60).in(Inches), RPM.of(3200).in(RPM));
     // flywheelSpeedMap.put(Inches.of(90).in(Inches), RPM.of(3600).in(RPM));
     // flywheelSpeedMap.put(Inches.of(120).in(Inches), RPM.of(4100).in(RPM));
-    // flywheelSpeedMap.put(Inches.of(150).in(Inches), RPM.of(4300).in(RPM));e
+    // flywheelSpeedMap.put(Inches.of(150).in(Inches), RPM.of(4300).in(RPM));
     // flywheelSpeedMap.put(Inches.of(187.8).in(Inches), RPM.of(5200).in(RPM));
-    flywheelSpeedMap.put(Inches.of(187.8).in(Inches), RPM.of(4250).in(RPM));
+    flywheelSpeedMap.put(Inches.of(190).in(Inches), RPM.of(4200).in(RPM));
     flywheelSpeedMap.put(Inches.of(180).in(Inches), RPM.of(4125).in(RPM));
     flywheelSpeedMap.put(Inches.of(165.1).in(Inches), RPM.of(4100).in(RPM));
     flywheelSpeedMap.put(Inches.of(165).in(Inches), RPM.of(4000).in(RPM));
-    flywheelSpeedMap.put(Inches.of(0).in(Inches), RPM.of(4000).in(RPM));
+    flywheelSpeedMap.put(Inches.of(160).in(Inches), RPM.of(3800).in(RPM));
+    flywheelSpeedMap.put(Inches.of(150).in(Inches), RPM.of(3700).in(RPM));
+    flywheelSpeedMap.put(Inches.of(140).in(Inches), RPM.of(3600).in(RPM));
+    flywheelSpeedMap.put(Inches.of(120).in(Inches), RPM.of(3400).in(RPM));
+    flywheelSpeedMap.put(Inches.of(0).in(Inches), RPM.of(3400).in(RPM));
   }
 }
